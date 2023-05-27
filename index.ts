41472c8325873be6b9f84dc9a1a52ae0b5fd5ce4
Yara Dallasheh <dallasheh@brainsome.de>
2023-05-27 16:51:29 +0200

    simple api

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
+import bodyParser from "body-parser";
 import express, { Request, Response } from "express";
+import { v4 as uuid } from "uuid";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.use(bodyParser.text());
+
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  const id = uuid();
+  res.send(id);
 });
 
 app.listen(port, () => {
