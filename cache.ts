1e978ee16c0496461f9be3be868036a426582cde
Yara Dallasheh <dallasheh@brainsome.de>
2023-05-27 17:44:21 +0200

    redis cache connection

--- cache.ts
+++ cache.ts
@@ -1,9 +1,23 @@
-const cache = new Map<string, string>();
+import { createClient, RedisClientType } from "redis";
 
-export function getFromCache(key: string): string | undefined {
-  return cache.get(key);
+let redisCache: RedisClientType;
+
+export async function redisConnect() {
+  redisCache = createClient();
+  redisCache.on("error", (err) => console.log("Redis Client Error", err));
+  await redisCache.connect();
 }
 
-export function setToCache(key: string, value: string) {
-  cache.set(key, value);
+export async function redisDisconnect() {
+  if (redisCache) {
+    await redisCache.disconnect();
+  }
+}
+
+export async function getFromCache(key: string): Promise<string | null> {
+  return await redisCache.get(key);
+}
+
+export async function setToCache(key: string, value: string) {
+  await redisCache.set(key, value);
 }
--- index.ts
+++ index.ts
@@ -1,16 +1,23 @@
 import bodyParser from "body-parser";
 import express, { Request, Response } from "express";
 import { v4 as uuid } from "uuid";
-import { getFromCache, setToCache } from "./cache";
+import {
+  getFromCache,
+  redisConnect,
+  redisDisconnect,
+  setToCache,
+} from "./cache";
+
+redisConnect();
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.text());
 
-app.post("/", (req: Request, res: Response) => {
+app.post("/", async (req: Request, res: Response) => {
   console.log(req.body);
-  let value = getFromCache(req.body);
+  let value = await getFromCache(req.body);
   if (!value) {
     value = uuid();
     setToCache(req.body, value);
@@ -21,3 +28,18 @@ app.post("/", (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+let shuttingDown = false;
+async function cleanup() {
+  if (shuttingDown) return;
+  shuttingDown = true;
+
+  console.log("Shutting down gracefully");
+
+  await redisDisconnect();
+
+  process.exit(0);
+}
+
+process.on("SIGINT", cleanup);
+process.on("SIGTERM", cleanup);
